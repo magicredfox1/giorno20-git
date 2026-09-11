6601211886c67581f92b96d7ad16e95ca5f24358
aurelio <magicredfox1@gmail.com>
2026-09-11 19:53:27 +0200

    completo

--- programma.py
+++ programma.py
@@ -1,6 +1,8 @@
 nome = input("Come ti chiami? ")
 eta = int(input("Quanti anni hai? "))
+gioco = input("Qual è il tuo gioco preferito? ")
 
 print(f"Ciao {nome}!")
 print(f"Hai {eta} anni.")
+print(f"Il tuo gioco preferito è {gioco}.")
 print("Questo è il mio primo progetto Git.")
